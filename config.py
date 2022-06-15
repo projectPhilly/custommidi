4f497ce6205758683b82048e42c33c41f9155b32
Lucy Guida <lguida@freshtechnology.com>
2022-06-15 13:27:49 -0400

    prepping for deployment

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	MIDI_FILES_PATH = os.path.join(basedir, 'app', 'midifiles')
+	MIDI_FILES_PATH = os.path.join(basedir, 'app', 'midifiles1')
 	MAX_CONTENT_LENGTH = 1024*1024*10 # 10 MB
 	UPLOAD_EXTENSIONS = ['.mid']
