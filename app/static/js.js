b146dddab5fdff01922df830ae04ab8bc2fa2f75
Lucy Guida <lguida@freshtechnology.com>
2022-06-15 11:42:04 -0400

    Add Search function to home page

--- app/static/js.js
+++ app/static/js.js
@@ -1,5 +1,25 @@
-$(document).ready(function() {
-	$('tr').click(function() {
+$(document).ready(function () {
+	$('tr').click(function () {
 		$(this).next('.details-row').toggleClass('tr-collapse');
+	});
+
+	$('.live-search-list li').each(function () {
+		$(this).attr('data-search-term', $(this).text().toLowerCase());
+	});
+
+	$('.live-search-box').on('keyup', function () {
+
+		var searchTerm = $(this).val().toLowerCase();
+
+		$('.live-search-list li').each(function () {
+
+			if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+
+		});
+
 	});
 });
